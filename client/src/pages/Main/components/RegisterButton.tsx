b10c9a936a57d3d4c65b766b62ff71790ce270c9
Matheus Fontana <mbfontana@outlook.com>
2022-09-01 10:27:13 -0300

    First draft registration dialog

--- client/src/pages/Main/components/RegisterButton.tsx
+++ client/src/pages/Main/components/RegisterButton.tsx
@@ -1,28 +1,39 @@
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Button, Slide, Stack, TextField, Typography } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-
 import { GreenButton } from "../../../components/GreenButton";
+import { useState, forwardRef, useEffect } from "react";
+import { RegisterDialog } from "./RegisterDialog";
 
 export const RegisterButton = () => {
+  const [open, setOpen] = useState(true);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
-    <GreenButton
-      sx={{
-        width: "85%",
-        justifyContent: "flex-start",
-        borderRadius: 5,
-      }}
-    >
-      <Stack
-        direction="row"
-        justifyContent="flex-start"
-        alignItems="center"
-        spacing={2}
+    <>
+      <GreenButton
+        sx={{
+          width: "85%",
+          justifyContent: "flex-start",
+          borderRadius: 5,
+        }}
+        onClick={handleOpen}
       >
-        <AddIcon sx={{ color: "#fff" }} />
-        <Typography color="white" variant="body2">
-          Cadastrar
-        </Typography>
-      </Stack>
-    </GreenButton>
+        <Stack
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          spacing={2}
+        >
+          <AddIcon sx={{ color: "#fff" }} />
+          <Typography color="white" variant="body2">
+            Cadastrar
+          </Typography>
+        </Stack>
+      </GreenButton>
+      <RegisterDialog open={open} />
+    </>
   );
 };
--- client/src/pages/Main/components/UserInfo.tsx
+++ client/src/pages/Main/components/UserInfo.tsx
@@ -1,9 +1,31 @@
-import { Avatar, Button, Stack, Typography, Box } from "@mui/material";
-
+import {
+  Avatar,
+  Button,
+  Stack,
+  Typography,
+  Box,
+  IconButton,
+} from "@mui/material";
 import { theme } from "../../../global/theme";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { useState } from "react";
+
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import InboxIcon from "@mui/icons-material/Inbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
 
 export const UserInfo = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
     <Stack
       direction="row"
@@ -14,21 +36,52 @@ export const UserInfo = () => {
     >
       <Avatar alt="" src="" />
       <Stack
-        direction="row"
+        direction="column"
         justifyContent="flex-start"
         alignItems="center"
         spacing={0}
       >
-        <Typography
-          variant="body2"
-          sx={{ color: theme.palette.text.secondary }}
+        <Stack
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          spacing={0}
         >
-          Valmir Advocacia
-        </Typography>
-        <ExpandMoreIcon
-          sx={{ color: theme.palette.text.secondary, width: "20px" }}
-        />
+          <Typography
+            variant="body2"
+            sx={{ color: theme.palette.text.secondary }}
+          >
+            Valmir Advocacia
+          </Typography>
+          <IconButton onClick={handleOpen}>
+            <ExpandMoreIcon
+              sx={{ color: theme.palette.text.secondary, width: "20px" }}
+            />
+          </IconButton>
+        </Stack>
       </Stack>
+      {open === true ? (
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <DraftsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Drafts" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      ) : (
+        <></>
+      )}
     </Stack>
   );
 };
