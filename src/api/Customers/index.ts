f4152d9b85f48d9051fbe475c6ff90808a6a7f9e
mbfontana <mbfontana@outlook.com>
2023-08-07 14:16:41 -0300

    Added customer api calls

--- src/api/Customers/index.ts
+++ src/api/Customers/index.ts
@@ -1,5 +1,35 @@
 import { MainAPI } from "../AuthenticatedAxios";
-import { GetAllCustomersResponse } from "./types";
+import {
+  CreateCustomer,
+  GetAllCustomersResponse,
+  GetCustomerResponse,
+} from "./types";
+
+export const getCustomerById = (customerId: number) =>
+  MainAPI.get<GetCustomerResponse>(`/customers/${customerId}`);
 
 export const getAllCustomers = () =>
   MainAPI.get<GetAllCustomersResponse[]>("/customers");
+
+export const registerCustomer = (
+  firstName,
+  lastName,
+  phone,
+  birth,
+  email,
+  cpf
+) => {
+  return MainAPI.post<CreateCustomer>("/customers", {
+    firstName: firstName,
+    lastName: lastName,
+    phone: phone,
+    birth: birth,
+    email: email,
+    cpf: cpf,
+  });
+};
+
+export const deleteCustomer = (cpf) =>
+  MainAPI.delete("/customers", {
+    data: { cpf },
+  });
--- src/api/Customers/types.ts
+++ src/api/Customers/types.ts
@@ -1,3 +1,41 @@
+export type CityObject = {
+  id: number;
+  name: string;
+  province: string;
+  font: number;
+};
+
+export type ConditionObject = {
+  id: number;
+  type: string;
+  value: number;
+};
+
+export type AreaObject = {
+  id: number;
+  metreage: number;
+  condition: ConditionObject;
+};
+
+export type PropertyObject = {
+  id: number;
+  name: string;
+  nirf: string;
+  city: CityObject;
+  areas: AreaObject[];
+};
+
+export type GetCustomerResponse = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  cpf: string;
+  phone: string;
+  birth: string;
+  properties: PropertyObject[];
+};
+
 export type GetAllCustomersResponse = {
   id: number;
   firstName: string;
@@ -8,4 +46,13 @@ export type GetAllCustomersResponse = {
   birth: string;
   createdAt: string;
   updatedAt: string;
+};
+
+export type CreateCustomer = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  cpf: string;
+  phone: string;
+  birth: string;
 };
